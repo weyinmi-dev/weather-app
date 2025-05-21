fa6645f1339ffdef836dd4baf97514e619758e6e
weyinmi-dev <weyinmi.e.madamedon@gmail.com>
2025-05-21 16:02:31 +0100

    creation of the weatherApp.tsx, weatherApp.css, and setup

---
+++ src/Components/WeatherApp.jsx
@@ -0,0 +1,160 @@
+import sunny from "../assets/images/sunny.png";
+import rainy from "../assets/images/rainy.png";
+import cloudy from "../assets/images/cloudy.png";
+import snowy from "../assets/images/snowy.png";
+import { useEffect, useState } from "react";
+
+const WeatherApp = () => {
+  const [data, setData] = useState([]);
+  const api_key = import.meta.env.VITE_API_KEY;
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const defaultLocation = "Lagos";
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&appid=${api_key}`;
+      const res = await fetch(url);
+      const defaultData = await res.json();
+      setData(defaultData);
+    };
+
+    fetchWeatherData();
+  }, []);
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
+
+  const weatherImages = {
+    Clear: sunny,
+    Rain: rainy,
+    Clouds: cloudy,
+    Snow: snowy,
+    Haze: cloudy,
+    Mist: cloudy,
+  };
+
+  const weatherImage = data.weather
+    ? weatherImages[data.weather[0].main]
+    : null;
+
+  const search = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
+    const res = await fetch(url);
+    const searchData = await res.json();
+    console.log(searchData);
+    setData(searchData);
+    setLocation("");
+  };
+
+  const backgroundImages = {
+    Clear: "linear-gradient(to right, #f3b07c, #fcd283)",
+    Clouds: "linear-gradient(to right, #57d6d4, #71eeec)",
+    Rain: "linear-gradient(to right, #5bc8fb, #80eaff)",
+    Snow: "linear-gradient(to right, #aff2ff, #ffffff)",
+    Haze: "linear-gradient(to right, #57d6d4, #71eeec)",
+    Mist: "linear-gradient(to right, #57d6d4, #71eeec)",
+  };
+
+  const backgroundImage = data.weather
+    ? backgroundImages[data.weather[0].main]
+    : "linear-gradient(to right, #f3b07c, #fcd283)";
+
+  const currentDate = new Date();
+
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const monthsOfYear = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const day = daysOfWeek[currentDate.getDay()];
+  const date = currentDate.getDate();
+  const month = monthsOfYear[currentDate.getMonth()];
+
+  const formattedDate = `${day}, ${date} ${month}`;
+
+  return (
+    <div className="container" style={{ backgroundImage }}>
+      <div
+        className="weather-app"
+        style={{
+          backgroundImage:
+            backgroundImage && backgroundImage.replace
+              ? backgroundImage && backgroundImage.replace("to right", "to top")
+              : null,
+        }}
+      >
+        <div className="search">
+          <div className="search-top">
+            <i className="fa-solid fa-location-dot"></i>
+            <div className="location">{data.name}</div>
+          </div>
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search for a city..."
+              value={location}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+            <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
+          </div>
+        </div>
+        <div className="weather">
+          <img src={weatherImage} alt="sunny" />
+          <div className="weather-type">
+            {data.weather ? data.weather[0].main : null}
+          </div>
+          <div className="temp">
+            {data.main ? `${Math.floor(data.main.temp / 10)}°C` : null}
+          </div>
+        </div>
+        <div className="weather-date">
+          <p>{formattedDate}</p>
+        </div>
+        <div className="weather-data">
+          <div className="humidity">
+            <div className="data-name">Humidity</div>
+            <i className="fa-solid fa-droplet"></i>
+            <div className="data">{data.main ? data.main.humidity : null}%</div>
+          </div>
+          <div className="wind">
+            <div className="data-name">Wind</div>
+            <i className="fa-solid fa-wind"></i>
+            <div className="data">
+              {data.wind ? data.wind.speed : null} km/h
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherApp;
